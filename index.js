f537450bf527deed4c720aff26c017070a53c7c6
LeilaSchooley <vortex962@zoho.com>
2021-11-24 23:43:50 +0000

    Added function that gets data from API and returns it in an object

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 async function getWeather() {
-  let weatherData = await fetch(
+  let response = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3db17fc135c2cd780b50535bedd2f485",
     { mode: "cors" }
   );
-  const jsonWeather = await weatherData.json();
-  console.log(jsonWeather);
+  const jsonWeather = await response.json();
+  let location = jsonWeather["name"];
+  let windspeed = jsonWeather["wind"]["speed"];
+  let temperature = jsonWeather["main"]["temp"];
+  let humidity = jsonWeather["main"]["humidity"];
+
+  let weatherData = {
+    location: location,
+    windspeed: windspeed,
+    temperature: temperature,
+    humidity: humidity,
+  };
 }
 getWeather();
